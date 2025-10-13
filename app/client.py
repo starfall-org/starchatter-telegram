f9818fe4302119025fdf810f3b3849cec9cb9234
Org Contributor <tonglanhchua@gmail.com>
2025-10-13 06:45:41 +0000

    Fix plugin path in client.py to use the correct relative directory

--- app/client.py
+++ app/client.py
@@ -1,12 +1,11 @@
-from pyrogram import Client
-
 from config import API_HASH, API_ID, BOT_TOKEN
+from pyrogram import Client
 
 client = Client(
     "starchatter",
     API_ID,
     API_HASH,
     bot_token=BOT_TOKEN,
-    plugins={"root": "app/handlers"},
+    plugins={"root": "handlers"},
     in_memory=True,
 )
